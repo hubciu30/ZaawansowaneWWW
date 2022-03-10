545a61999ab21541604c03fdd3d1438f1f8fcbc6
hubciu30 <szkolny98@wp.pl>
2022-03-10 12:49:07 +0100

    add API references

--- aplikacja/server.js
+++ aplikacja/server.js
@@ -1,11 +1,23 @@
-const express = require('express')
+const express = require('express');
+const http = require('http');
 const app = express()
-const port = 3000
+
+const httpServer = http.createServer(app);
+const {port} = require('./app/config');
+
+// aplication/json
+app.use(express.json());
+
+// API import
+require('./app/api/v1/login.js')(app);
+require('./app/api/v1/signup.js')(app);
+require('./app/api/v1/home.js')(app);
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.redirect('/home');
 })
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Start serwera na porcie: ${port}`)
 })
