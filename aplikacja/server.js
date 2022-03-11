13cf88f97f9859462b49ba8e7c63fee65ee1d28d
hubciu30 <szkolny98@wp.pl>
2022-03-11 15:32:00 +0100

    set static files and add support aplication/urlencoded

--- aplikacja/server.js
+++ aplikacja/server.js
@@ -7,6 +7,11 @@ const {port} = require('./app/config');
 
 // aplication/json
 app.use(express.json());
+// aplication/urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// share public file
+app.use(express.static(__dirname+"/app/public"))
 
 // API import
 require('./app/api/v1/login.js')(app);
