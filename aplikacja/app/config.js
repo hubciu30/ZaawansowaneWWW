29cbc747bbd02c011afdbadd8b944cf1022c503b
hubciu30 <szkolny98@wp.pl>
2022-03-11 15:37:11 +0100

    add path to home

--- aplikacja/app/config.js
+++ aplikacja/app/config.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 module.exports = {
+    home_path: __dirname,
     port: process.env.PORT,
-    db_host: process.DATABASE_HOST,
-    db_user: process.DATABASE_USER,
-    db_password: process.DATABASE_PASSWORD,
-    db_name: process.DATABASE_NAME
+    db_host: process.env.DATABASE_HOST,
+    db_user: process.env.DATABASE_USER,
+    db_password: process.env.DATABASE_PASSWORD,
+    db_name: process.env.DATABASE_NAME
 }
