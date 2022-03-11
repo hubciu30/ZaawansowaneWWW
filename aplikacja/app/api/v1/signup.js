3738cc591ce69957cfe526db39d60a8b5850faaa
hubciu30 <szkolny98@wp.pl>
2022-03-11 15:36:05 +0100

    add registration option

--- aplikacja/app/api/v1/signup.js
+++ aplikacja/app/api/v1/signup.js
@@ -1,6 +1,41 @@
+const crypto = require('crypto');
+const {home_path} = require('../../config')
 module.exports = (app) =>
 {
     app.get('/signup', (req, res)=>{
-        res.send("Signup")
-    })
+        console.log(req.query)
+        res.sendFile(home_path+'/views/signup.html')
+    });
+
+    app.post('/signup', async (req, res)=>{
+        console.log(req.body)
+        if(req.body.login && req.body.password && req.body.login.length > 2){
+            const db = require('../../database/database');
+            const salt = crypto.randomBytes(16).toString('hex');
+            const hash = crypto.createHash('sha256').update(req.body.password+salt).digest('hex');
+            const time = Date.now();
+            try
+            {
+                let response = await db.Users.get(null, req.body.login);
+                console.log(response);
+                if(response.length === 0){
+                    response = await db.query('INSERT INTO `users` (`id`, `username`, `password`, `salt`, `create_time`, `last_login_time`) VALUES (NULL, ?, ?, ?, ?, ?)', [req.body.login, hash, salt, time, 0,])
+                    response = await db.query('INSERT INTO `roles` (`id`, `user_id`, `name`, `power`) VALUES (NULL, ?, ?, ?)', [response.insertId, "User", 1]);
+                    res.sendStatus(200);
+                }
+                else{
+                    res.redirect('/signup?status=2');
+                }
+
+                //res.redirect('/login');
+            }
+            catch(e){
+                console.log(e)
+                res.sendStatus(500);
+            }
+        }
+        else{
+            res.redirect('/signup?status=1');
+        }
+    });
 }
