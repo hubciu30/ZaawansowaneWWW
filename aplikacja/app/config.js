be5c162b329b888d081090fe0a7b735d3c7ac9e5
hubciu30 <szkolny98@wp.pl>
2022-03-13 18:20:26 +0100

    add session support

--- aplikacja/app/config.js
+++ aplikacja/app/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 module.exports = {
     home_path: __dirname,
+    session_duration: 1000*60*60, // 1h
     port: process.env.PORT,
     db_host: process.env.DATABASE_HOST,
     db_user: process.env.DATABASE_USER,
