864bbd8b7604305b9a1a68f6e14b4b8003a7fb52
hubciu30 <szkolny98@wp.pl>
2022-03-19 15:37:48 +0100

    add routing to new endpoints

--- aplikacja/server.js
+++ aplikacja/server.js
@@ -9,16 +9,18 @@ const {port} = require('./app/config');
 app.use(express.json());
 // aplication/urlencoded
 app.use(express.urlencoded({ extended: true }));
-
 // share public file
 app.use(express.static(__dirname+"/app/public"))
 
 // API import
-require('./app/api/v1/login.js')(app);
-require('./app/api/v1/signup.js')(app);
-require('./app/api/v1/logout.js')(app);
-require('./app/api/v1/home.js')(app);
-
+require('./app/api/v1/login')(app);
+require('./app/api/v1/signup')(app);
+require('./app/api/v1/logout')(app);
+require('./app/api/v1/home')(app);
+require('./app/api/v1/users')(app);
+require('./app/api/v1/categories')(app);
+require('./app/api/v1/topics')(app);
+require('./app/api/v1/posts')(app);
 
 
 app.get('/', (req, res) => {
